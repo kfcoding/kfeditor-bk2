e70235c684b769cecf1ba16e615509c08d016d69
zhangliqing <623009470@qq.com>
2018-05-13 11:10:37 +0800

    add editor component

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.css';
+import Editor from '../components/Editor';
 import { Layout, Menu, Dropdown, Icon } from 'antd';
-
+import {Value} from 'slate';
 const {Header, Content, Footer, Sider} = Layout;
 const {SubMenu} = Menu;
+
+const initialValue = Value.fromJSON({
+  document: {
+    nodes: [
+      {
+        object: 'block',
+        type: 'paragraph',
+        nodes: [
+          {
+            object: 'text',
+            leaves: [
+              {
+                text: 'A line of text in a paragraph.',
+              }
+            ],
+          },
+        ],
+      },
+    ],
+  },
+});
+
 const menu = (
   <Menu>
     <Menu.Item key="0">
@@ -70,7 +93,7 @@ function IndexPage() {
             </Menu>
           </Sider>
           <Content style={{padding: '0 24px', minHeight: 980}}>
-            Content
+            <Editor content={initialValue}/>
           </Content>
         </Layout>
       </Content>
